da3412434aa1be9bc5cf7e373614559aa9cbcc76
Leandro Arguello <leanarguello012@gmail.com>
2020-08-27 19:51:15 -0300

    Agrego componente Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 //importacion
 import React from 'react'
-import Button from './components/Button'
-import MathOperations from './components/MathOperations'
-import Result from './components/Result'
 import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
+import Result from './components/Result'
 import './App.css'
+
 
 //generación de la función del componente
 const App = () => {
-
-    const clickHandlerFunction = text => {
-        console.log(text)
-    }
 
     const onClickOperationFunction = operation => {
         console.log("operation:", operation)
@@ -29,25 +26,18 @@ const App = () => {
         console.log("delete:", del)
     }
 
+    const onClickNumberFunction = number => {
+        console.log("Number:", number)
+    }
+
 
     //Lo que ejecuta la función
     return (
         <main className="react-calculator">
             <Result value={undefined} />
-            <div className="numbers">
-                <Button text="1" clickHandler={clickHandlerFunction} />
-                <Button text="2" clickHandler={clickHandlerFunction} />
-                <Button text="3" clickHandler={clickHandlerFunction} />
-                <Button text="4" clickHandler={clickHandlerFunction} />
-                <Button text="5" clickHandler={clickHandlerFunction} />
-                <Button text="6" clickHandler={clickHandlerFunction} />
-                <Button text="7" clickHandler={clickHandlerFunction} />
-                <Button text="8" clickHandler={clickHandlerFunction} />
-                <Button text="9" clickHandler={clickHandlerFunction} />
-                <Button text="0" clickHandler={clickHandlerFunction} />
-            </div>
+            <Numbers onClickNumber={onClickNumberFunction} />            
             <Functions onContentClear={onContentClearFunction} onDelete={onDeleteFunction} />
-            < MathOperations onClickOperation={onClickOperationFunction} onClickEqual={onClickEqualFunction} />
+            <MathOperations onClickOperation={onClickOperationFunction} onClickEqual={onClickEqualFunction} />
         </main>)
 }
 
